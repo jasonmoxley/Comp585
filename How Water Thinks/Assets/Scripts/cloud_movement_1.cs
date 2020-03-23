2a1f8a0848b632ec67136d7765cb748cb5de62ff
colbear123 <ckilmart@live.unc.edu>
2020-03-22 22:02:44 -0400

    added scripts
    cloud movement and sprit animations, basic. not a all complicated yet

---
+++ How Water Thinks/Assets/Scripts/cloud_movement_1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour {
+
+ 
+    public Vector2 begin;
+    public Vector2 end;
+    public float time_for_move;
+ 
+    float timer;
+ 
+    // Use this for initialization
+    void Start () {
+ 
+        transform.position = begin;
+        timer = time_for_move;
+ 
+    }
+ 
+    // Update is called once per frame
+    void Update () {
+ 
+        timer -= Time.deltaTime;
+ 
+        if (timer > 0) {
+            Vector2 distance =  end - begin;
+                        float degree_of_movement = (time_for_move - timer) / time_for_move;
+                        transform.position = new Vector2 (begin.x + (distance.x * degree_of_movement), begin.y + (distance.y * degree_of_movement));
+                }
+    }
+}
