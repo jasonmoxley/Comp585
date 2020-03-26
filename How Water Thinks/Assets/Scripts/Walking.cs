9873b687ed45b05bb43974b686d658b65616a527
jasonmoxley <jmoxley@live.unc.edu>
2020-03-25 22:07:20 -0400

    hooking up all the scenes
    redesigning main menu. hooking up the buttons. adding scene order

--- How Water Thinks/Assets/Scripts/Walking.cs
+++ How Water Thinks/Assets/Scripts/Walking.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Walking : MonoBehaviour {
      
-     float speed = 1.0f;
-     
-     void Update() {
-         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-         transform.position += move * speed * Time.deltaTime;
-     }
- }
+    float speed = 1.0f;
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Level 0");
+    }
+    public void checkForSceneChange()
+    {
+        if (GameObject.FindGameObjectWithTag("Lion").transform.position.x >= -10)
+        {
+            NextScene();
+        }
+    }
+    void Update() {
+        checkForSceneChange();
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
